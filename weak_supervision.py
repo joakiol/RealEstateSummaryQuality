64b639a75aec9a24c051a3f3e5c32a57e853cd1d
Joakim Olsen <joakiol@stud.ntnu.no>
2021-01-20 15:02:14 +0100

    add docstrings

--- weak_supervision.py
+++ weak_supervision.py
@@ -11,10 +11,9 @@ import pickle
 
 from labeling_functions import LABELING_FUNCTIONS, prepare_data_for_labeling_functions
 
-class GenerativeModel:
-    """Class for weak supervision generative model, used for making labels. """
+class LabelModel:
     def __init__(self):
-        """Initialize"""
+        """Class for weak supervision label model, used for making labels."""        
         self.labeling_functions = LABELING_FUNCTIONS
         self.applier = PandasLFApplier(lfs=self.labeling_functions)
         self.df = None
@@ -22,32 +21,42 @@ class GenerativeModel:
         self.model = None
 
     def __call__(self, data):
-        """
-        Takes iterable of SummaryReport objects as input, returns a list of probabilistic labels.
-        That is, this function both trains the model on the data, and then finds probabilities. 
-        """
+        """Takes iterable of SummaryReport objects as input, returns a list of probabilistic
+        labels. This function both trains the model on the data, and then finds probabilities. 
+
+        Args:
+            data (iterable[SummaryReport]): Data to label. 
+
+        Returns:
+            pd.DataFrame: Probabilistic labels for data, with ids in index. 
+        """        
         self.train(data)
         return self.predict_training_set()
 
     def train(self, data):
-        """
-        Trains the model on the input data. 
-        
-        :param data: VenduData-object, containing data for training the generative model. 
-        """
+        """Trains the model on the input data. 
+
+        Args:
+            data (iterable[SummaryReport]): Data for training label model. 
+        """        
         self.df = self._prepare_for_labeling(data)
         self.lambda_matrix = self.applier.apply(df=self.df)
         self.model = LabelModel(cardinality=2, verbose=True)
         self.model.fit(L_train=self.lambda_matrix, n_epochs=500, log_freq=100, seed=123)
 
     def predict(self, data):
-        """
-        Fit model on data.
+        """Fit model on data. If data is the same as the label model was trained on, 
+        it is much faster to use self.predict_training_set. 
 
-        :param data: VenduData-object, containing data for training the generative model. 
+        Args:
+            data (iterable[SummaryReport]): Data to predict labels on. 
 
-        :return: Probabilistic labels
-        """
+        Raises:
+            RuntimeError: If model has not been trained, it cannot predict labels. 
+
+        Returns:
+            pd.DataDrame: Probabilistic labels for data, with ids in index. 
+        """        
         if self.model == None:
             raise RuntimeError("Model must be trained before predict-method is called. ")
         df = self._prepare_for_labeling(data)
@@ -57,10 +66,15 @@ class GenerativeModel:
         return pd.DataFrame({'prob_bad': probs[:,0], 'prob_good': probs[:,1]}, index=df.index)
 
     def predict_training_set(self):
-        """
-        Predict labels on the set that was used for training the model. Much faster than calling 
+        """Predict labels on the set that was used for training the model. Much faster than calling 
         predict(data=training_data), since it takes time to prepare dataset for labeling functions. 
-        """
+
+        Raises:
+            RuntimeError: If model has not been trained, it cannot predict labels. 
+
+        Returns:
+            pd.DataDrame: Probabilistic labels for data, with ids in index. 
+        """        
         if self.model == None:
             raise RuntimeError("Model must be trained before predict-method is called. ")
         df = self.df
@@ -70,7 +84,12 @@ class GenerativeModel:
         return pd.DataFrame({'prob_bad': probs[:,0], 'prob_good': probs[:,1]}, index=df.index)
 
     def analyse_training_set(self, latexpath=None):
+        """Perform analysis of labeling functions. 
 
+        Args:
+            latexpath (str, optional): Save analysis to txt file, in latex table format. 
+                                       Defaults to None (no saving to file).
+        """
         analysis = LFAnalysis(L=self.lambda_matrix, lfs=self.labeling_functions).lf_summary()
         print(analysis)
         if latexpath != None:
@@ -84,7 +103,11 @@ class GenerativeModel:
 
 
     def plot_training_labels(self, name=None):
+        """Plot resulting labels for training set
 
+        Args:
+            name (str, optional): Filename for saving plot. Defaults to None (no saving).
+        """
         labels = self.predict_training_set()
 
         plt.figure(figsize=(8,5))
@@ -103,6 +126,14 @@ class GenerativeModel:
         return prepare_data_for_labeling_functions(data)
 
     def save(self, modelname='default'):
+        """Save label model. 
+
+        Args:
+            modelname (str, optional): Filename for saving. Defaults to 'default'.
+
+        Raises:
+            ValueError: Cannot save model if model has not been trained. 
+        """        
         path = 'models/WeakSupervision/%s' % modelname
         if self.model != None:
             Path(path).mkdir(parents=True, exist_ok=True)
@@ -112,6 +143,17 @@ class GenerativeModel:
             raise ValueError("Model has not been trained. Train model before saving. ")
 
     def load(self, modelname='default'):
+        """Load previously saved label model from file. 
+
+        Args:
+            modelname (str, optional): Filename to load from. Defaults to 'default'.
+
+        Raises:
+            NameError: Raises error if given modelname is not a prevously saved model. 
+
+        Returns:
+            LabelModel: self
+        """        
         path = 'models/WeakSupervision/%s' % modelname
         try:
             self.df = pd.read_csv('%s/df.csv' % path, index_col='id')
